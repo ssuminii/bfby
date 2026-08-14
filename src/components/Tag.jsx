4e366f112cf7293082de7aa10ef0a9341266f0c2
jizero <jizero27@gmail.com>
2026-08-14 13:27:16 +0900

    feat: 태그 컴포넌트 구현 (#5)

---
+++ src/components/Tag.jsx
@@ -0,0 +1,17 @@
+export default function Tag({
+  children,
+  bg = 'bg-info',
+  text = 'text-white',
+  weight = 'font-bold',
+  className = '',
+}) {
+  return (
+    <span
+      className={`inline-flex justify-center items-center gap-[10px] px-2
+        rounded-full text-center text-body2 leading-[1.5] tracking-tight-1
+        ${bg} ${text} ${weight} ${className}`}
+    >
+      {children}
+    </span>
+  )
+}
