872cf9821d0582c0229260390a9c4674d361543a
Sumin <tnalsdldkr2@naver.com>
2026-08-17 16:46:27 +0900

    feat: NavBar 컴포넌트 구현 및 라우트 추가 (#19)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Onboarding from './pages/Onboarding'
+import Home from './pages/Home'
+import Reports from './pages/Reports'
+import BuyOrNot from './pages/BuyOrNot'
 import Consult from './pages/Consult'
 import Report from './pages/Report'
 import ReasonSurvey from './pages/ReasonSurvey'
@@ -12,6 +15,9 @@ function App() {
           <div className="h-full">
             <Routes>
               <Route path="/" element={<Onboarding />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/reports" element={<Reports />} />
+              <Route path="/buyornot" element={<BuyOrNot />} />
               <Route path="/consult" element={<Consult />} />
               <Route path="/report" element={<Report />} />
               <Route path="/report/reason/:choice" element={<ReasonSurvey />} />
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import NavBar from '../components/NavBar'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col h-full bg-white">
+      <div className="flex-1" />
+      <NavBar />
+    </div>
+  )
+}
