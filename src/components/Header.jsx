4f86a9b682c637ddcf4a0ae2c31cc44daae4c42b
Sumin <tnalsdldkr2@naver.com>
2026-08-19 01:05:07 +0900

    style: Header 버튼 터치 영역 및 타이틀 정렬 수정 (#27)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,17 @@ export default function Header({ title, onBack }) {
   const navigate = useNavigate();
 
   return (
-    <header className="relative h-[58px] shrink-0 flex items-center justify-center bg-white">
+    <header className="relative h-[58px] shrink-0 flex items-center pl-2 bg-white">
       <button
         type="button"
         onClick={onBack ?? (() => navigate(-1))}
         aria-label="뒤로 가기"
-        className="absolute left-2 top-1/2 -translate-y-1/2 bg-transparent"
+        className="flex items-center justify-center size-12 shrink-0"
       >
         <ChevronLeftIcon className="block text-black" />
       </button>
       {title && (
-        <p className="absolute bottom-[10px] left-1/2 -translate-x-1/2 whitespace-nowrap text-title text-black">
+        <p className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 whitespace-nowrap text-title text-black">
           {title}
         </p>
       )}
