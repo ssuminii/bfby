72074634205d05fb86e18fc64bf2dacd7089f2c0
jizero <jizero27@gmail.com>
2026-08-21 00:36:47 +0900

    feat: 리포트 카드 제목에 아이콘 추가 (#52)
    카드 성격을 제목만으로 구분하기 어려워 아이콘을 함께 둔다.
    이유는 목록, 비용은 계산기, 내 기록은 시계, 선택지는 전구를 쓴다.
    
    전구는 시안에서 두 조각으로 나뉘어 있어 위치를 잡아 겹친다.

---
+++ src/components/report/CardIcon.jsx
@@ -0,0 +1,33 @@
+import bulbBase from '../../assets/icons/report/bulb-base.svg'
+import bulbBody from '../../assets/icons/report/bulb-body.svg'
+import calculator from '../../assets/icons/report/calculator.svg'
+import history from '../../assets/icons/report/history.svg'
+import list from '../../assets/icons/report/list.svg'
+
+// 카드 제목 앞에 붙는 아이콘. 카드 성격에 맞춰 고른다.
+const ICONS = {
+  reason: list,
+  cost: calculator,
+  history,
+}
+
+export default function CardIcon({ name }) {
+  // 전구는 두 조각으로 나뉘어 있어 위치를 잡아 겹친다
+  if (name === 'idea') {
+    return (
+      <span className='relative block size-6 shrink-0 overflow-clip'>
+        <img src={bulbBody} alt='' className='absolute inset-[12.5%_29.17%_33.33%_20.83%]' />
+        <img src={bulbBase} alt='' className='absolute bottom-[12.5%] left-[33.33%] right-[41.67%] top-3/4' />
+      </span>
+    )
+  }
+
+  const src = ICONS[name]
+  if (!src) return null
+
+  return (
+    <span className='relative block size-6 shrink-0 overflow-clip'>
+      <img src={src} alt='' className='absolute inset-0 size-full object-contain' />
+    </span>
+  )
+}
