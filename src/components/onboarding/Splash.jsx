3a92ff307739542954eb4de327f2bc27bd3d47fc
Sumin <tnalsdldkr2@naver.com>
2026-08-21 01:15:05 +0900

    feat: Splash 이미지 제거 및 텍스트 wave 애니메이션 추가 (#55)

--- src/components/onboarding/Splash.jsx
+++ src/components/onboarding/Splash.jsx
@@ -13,14 +13,25 @@ export default function Splash({ onNext }) {
       style={{ background: 'linear-gradient(to bottom, #ffffff 10%, #f1f2f5 50%, #f1f2f5 100%)' }}
     >
       <div
-        className='absolute left-[calc(50%-0.5px)] -translate-x-1/2 -translate-y-1/2 flex flex-col gap-[8px] items-center w-[244px]'
-        style={{ top: 'calc(50% - 81px)' }}
+        className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-2 items-center w-61'
+        style={{ top: 'calc(50% + 0.5px)' }}
       >
         <p
           className='text-body1 font-bold text-gray-800 text-center w-full'
           style={{ letterSpacing: '-0.16px', lineHeight: 1.45 }}
         >
-          후회 없는 소비를 위한 당신만의 비서
+          {'후회 없는 소비를 위한 당신만의 비서'.split('').map((char, i) => (
+            <span
+              key={i}
+              className='inline-block'
+              style={{
+                animation: 'wave-char 2s ease-in-out 1',
+                animationDelay: `${i * 0.07}s`,
+              }}
+            >
+              {char === ' ' ? ' ' : char}
+            </span>
+          ))}
         </p>
         <p
           className='text-gray-800 w-full'
@@ -32,22 +43,6 @@ export default function Splash({ onNext }) {
         >
           Before Buy
         </p>
-      </div>
-
-      <div
-        className='absolute left-1/2 -translate-x-1/2'
-        style={{ bottom: '-233px', width: '491px', height: '366px' }}
-      >
-        <div className='absolute' style={{ inset: '-8.2% -6.11%' }}>
-          <img src='/splash-ellipse.svg' alt='' className='block w-full h-full' />
-        </div>
-      </div>
-
-      <div
-        className='absolute left-1/2 -translate-x-1/2'
-        style={{ bottom: '89px', width: '82.64px', height: '196.79px' }}
-      >
-        <img src='/splash-character.svg' alt='' className='absolute inset-0 block w-full h-full' />
       </div>
     </div>
   )
