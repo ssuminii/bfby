a0fa0bac31262dc93baae40e1dd55b335b24adf1
Sumin <tnalsdldkr2@naver.com>
2026-08-22 01:56:09 +0900

    fix: dev 머지로 인한 카드 제목 정렬 수정

--- src/components/report/CardTitle.jsx
+++ src/components/report/CardTitle.jsx
@@ -2,7 +2,7 @@ import CardIcon from "./CardIcon";
 
 export default function CardTitle({ icon, children }) {
   return (
-    <div className="flex items-center justify-center gap-2">
+    <div className="flex items-center gap-2">
       <CardIcon name={icon} />
       <p className="text-head text-gray-800">{children}</p>
     </div>
